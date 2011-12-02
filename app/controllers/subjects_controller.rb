3e9e6609b38a4699423c0ae5615970e5971a2167
Tyrone Gaylord <tyrone.mdd@gmail.com>
2011-12-02 12:55:20 -0600

    added crud to subject controller and view

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -27,11 +27,44 @@ class SubjectsController < ApplicationController
     # this save the object
     if @subject.save
       #this is if statement for if the form saves
+      flash[:notice] = "You have created a new object"
       redirect_to(:action => 'list')
     else
       #if it doesnt go back to new action
       render('new')
     end
+  end
+  
+  def edit
+    # THIS THE UPDATE PART OF CRUD
+    @subject = Subject.find(params[:id])
+  end
+  
+  def update
+    # THIS THE UPDATE PART OF CRUD
+    # were going find a object using the params
+    @subject = Subject.find(params[:id])
+    # this save the object
+    if @subject.update_attributes(params[:subject])
+      #this is if statement for if the form saves
+      flash[:notice] = "You have updated a new object"
+      redirect_to(:action => 'show', :id => @subject.id)
+    else
+      #if it doesnt go back to new action
+      render('edit')
+    end
+  end
+  
+  def delete
+    # THIS THE DESTROY PART OF CRUD
+    @subject = Subject.find(params[:id])
+  end
+  
+  def destroy
+    # THIS THE DESTROY PART OF CRUD
+    flash[:notice] = "You have destoryed an object"
+    Subject.find(params[:id]).destroy
+    redirect_to(:action => 'list')
   end
   
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 SimpleCms::Application.routes.draw do
-  # resources :subjects
+  resources :subjects
+  # match 'subjects/list' => 'subjects#list', :as => "list"
   
-  root :to => "demo#index"
+  # root :to => "demo#index"
   
   #get "demo/index"
 
@@ -55,10 +56,11 @@ SimpleCms::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  root :to => "subjects#list", :as => "list"
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  match ':controller(/:action(/:id(.:format)))'
+  # match ':controller(/:action(/:id(.:format)))'
 end
